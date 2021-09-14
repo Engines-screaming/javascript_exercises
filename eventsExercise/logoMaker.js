ad039e108bcd806439a456fa2bb17da2d8005d67
Karl Ravago <rhaast@olympus.local>
2021-09-14 02:14:07 -0700

    working logoMaker

--- eventsExercise/logoMaker.js
+++ eventsExercise/logoMaker.js
@@ -1,6 +1,22 @@
 const form = document.querySelector("#logoform");
+const brandNameInput = document.querySelector("input[name='brandname']");
+const colorInput = document.querySelector("input[name='color']");
+const fontSizeInput = document.querySelector("input[name='fontsize']");
+const section = document.querySelector('#results');
 
 form.addEventListener('submit', function(e) {
     e.preventDefault();
-    console.log("Submitted!")
-}) 
+    createLogo(brandNameInput.value,
+        colorInput.value,
+        fontSizeInput.value);
+
+})
+
+function createLogo(brandname, color, fontsize) {
+    const logo = document.createElement("h2");
+    logo.innerText = brandname;
+    logo.style.color = color;
+    logo.style.fontSize = fontsize + 'px';
+
+    section.appendChild(logo);
+}
