e03557eaac6ca8423e017f6303e62e63aee7133e
Engines-screaming <karl.ravago@gmail.com>
2021-09-28 22:21:09 -0700

    added new test using matcher for throwing error

--- unit7/adder.js
+++ unit7/adder.js
@@ -1,3 +1,6 @@
 function addNums(num1, num2) {
+    if (!Number.isFinite(num1) || !Number.isFinite(num2)) {
+        throw new Error('Non numeric input')
+    }
     return num1 + num2;
 }
--- unit7/test.adder.js
+++ unit7/test.adder.js
@@ -1,7 +1,13 @@
 it('should add two positive numbers correctly', function() {
     expect(addNums(2, 2)).toEqual(4);
+    expect(addNums(40, 20)).toEqual(60);
 });
 
 it('should add two negative numbers correctly', function() {
+    expect(addNums(-2, -39)).toEqual(-41);
+    expect(addNums(-5,-55)).toEqual(-60);
+});
 
+it('should be able to throw an error for non-int args', function() {
+    expect(() => addNums('yo', 'sdlfkj')).toThrowError();
 });
