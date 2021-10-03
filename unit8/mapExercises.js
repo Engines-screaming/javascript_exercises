019d705458170a2025efc17700dbcab18682e821
Engines-screaming <karl.ravago@gmail.com>
2021-10-02 20:27:08 -0700

    made my own map function

--- unit8/mapExercises.js
+++ unit8/mapExercises.js
@@ -1,7 +1,28 @@
-const numbers = [21, 37, 64, 99, 142];
+// const numbers = [21, 37, 64, 99, 142];
 
-negatives = numbers.map(function(num) {
-    return num * -1;
-});
+// negatives = numbers.map(function(num) {
+//     return num * -1;
+// });
 
-console.log(negatives);
+// console.log(negatives);
+
+
+// attempt at my own map function
+
+function myMap(arr, func) {
+    let new_arr = [];
+
+    for (let item in arr) {
+        new_arr.push(func(item));
+    }
+
+    return new_arr
+}
+
+arr = [1, 2, 3];
+new_arr = myMap(arr, function(val) {
+    return val * 2;
+})
+
+console.log(new_arr);
+console.log(arr);
