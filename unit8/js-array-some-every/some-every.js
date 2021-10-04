59a25622d477595fccccc3e78f628ab80d51679c
Engines-screaming <karl.ravago@gmail.com>
2021-10-03 21:32:03 -0700

    completed hasNoDuplicates

--- unit8/js-array-some-every/some-every.js
+++ unit8/js-array-some-every/some-every.js
@@ -34,7 +34,11 @@ Examples:
     hasOnlyOddNumbers([1,2,3,5,7]) // false
 */
 
-function hasOnlyOddNumbers(arr) {}
+function hasOnlyOddNumbers(arr) {
+    return arr.every(function(val) {
+        return val % 2 !== 0;
+    })
+}
 
 /*
 Write a function called hasNoDuplicates which accepts an array and returns true if there are no duplicate values (more than one element in the array that has the same value as another). If there are any duplicates, the function should return false.
@@ -44,7 +48,15 @@ Examples:
     hasNoDuplicates([1,2,3]) // true
 */
 
-function hasNoDuplicates(arr) {}
+function hasNoDuplicates(arr) {
+    return arr.every(function(val) {
+        filtered = arr.filter(function(item) {
+            return item === val;
+        });
+
+        return filtered.length === 1;
+    });
+}
 
 /*
 Write a function called hasCertainKey which accepts an array of objects and a key, and returns true if every single object in the array contains that key. Otherwise it should return false.
