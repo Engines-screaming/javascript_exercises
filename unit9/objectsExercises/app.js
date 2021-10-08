ac84fdac213ec2bc1c90a35cd2de30ef3ea0e0a7
Engines-screaming <karl.ravago@gmail.com>
2021-10-08 16:05:49 -0700

    completed objects exercise

--- unit9/objectsExercises/app.js
+++ unit9/objectsExercises/app.js
@@ -24,3 +24,23 @@ let instructor = {
     sayHi() {return 'Hi';}, 
     sayBye() {return this.firstName + ' says bye!';}
 }
+
+// createAnimal function
+// Write a function which generates an animal object. The function should accepts 3 arguments:
+    // species: the species of animal (‘cat’, ‘dog’)
+    // verb: a string used to name a function (‘bark’, ‘bleet’)
+    // noise: a string to be printed when above function is called (‘woof’, ‘baaa’)
+// Use one or more of the object enhancements we’ve covered.
+
+function createAnimal(species, verb, noise) {
+    return {species, [verb]() {return noise}};
+}
+
+const d = createAnimal("dog", "bark", "Woooof!")
+// {species: "dog", bark: ƒ}
+d.bark()  //"Woooof!"
+
+const s = createAnimal("sheep", "bleet", "BAAAAaaaa")
+// {species: "sheep", bleet: ƒ}d
+s.bleet() //"BAAAAaaaa"
+
