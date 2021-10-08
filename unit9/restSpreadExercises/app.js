d44f252d19a526c8a01534dc1336b7a615ba9475
Engines-screaming <karl.ravago@gmail.com>
2021-10-07 23:49:40 -0700

    filterOutOdds and findMin exercises completed

--- unit9/restSpreadExercises/app.js
+++ unit9/restSpreadExercises/app.js
@@ -6,3 +6,12 @@
 //     });
 //   }
 
+const filterOutOdds = (...numbers) => numbers.filter(num => num % 2 === 0);
+
+// Write a function called findMin that accepts a variable number of arguments and returns the smallest argument.
+// Make sure to do this using the rest and spread operator.
+// findMin(1,4,12,-3) // -3
+// findMin(1,-1) // -1
+// findMin(3,1) // 1
+
+const findMin = (...args) => args.reduce((acc, val) => (acc < val ? acc : val), args[0])
