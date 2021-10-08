e97f0a0c670dc27f760dd0835d9f50ea53dbfe34
Engines-screaming <karl.ravago@gmail.com>
2021-10-07 19:08:43 -0700

    finished arrow function exercise

--- unit9/arrowFunctionsExercise/app.js
+++ unit9/arrowFunctionsExercise/app.js
@@ -1,19 +1,20 @@
 // convert this function into a one liner:
-function double(arr) {
-    return arr.map(function(val) {
-      return val * 2;
-    });
-  }
+// function double(arr) {
+//     return arr.map(function(val) {
+//       return val * 2;
+//     });
+//   }
 
-  
-// refactor to use arrow functions
-function squareAndFindEvens(numbers){
-    var squares = numbers.map(function(num){
-      return num ** 2;
-    });
-    var evens = squares.filter(function(square){
-      return square % 2 === 0;
-    });
-    return evens;
-  }
-  
+const double = arr => arr.map(val => val * 2);
+// // refactor to use arrow functions
+// function squareAndFindEvens(numbers){
+//     var squares = numbers.map(function(num){
+//       return num ** 2;
+//     });
+//     var evens = squares.filter(function(square){
+//       return square % 2 === 0;
+//     });
+//     return evens;
+//   }
+
+  const squareAndFindEvens = numbers => numbers.map(num => num ** 2).filter(square => square % 2 === 0);
