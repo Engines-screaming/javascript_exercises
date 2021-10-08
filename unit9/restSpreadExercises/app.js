5139d92957202957d3957d06fb950205d07bbac0
Engines-screaming <karl.ravago@gmail.com>
2021-10-07 23:22:27 -0700

    added rest spread exercise files

---
+++ unit9/restSpreadExercises/app.js
@@ -0,0 +1,8 @@
+// refactor to use rest/spread operator and arrow functions
+// function filterOutOdds() {
+//     var nums = Array.prototype.slice.call(arguments);
+//     return nums.filter(function(num) {
+//       return num % 2 === 0
+//     });
+//   }
+
