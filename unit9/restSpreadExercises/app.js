5647653f05644e01c884c9c5daf0a7f9d61f1991
Engines-screaming <karl.ravago@gmail.com>
2021-10-08 00:59:13 -0700

    completed mergeObjects

--- unit9/restSpreadExercises/app.js
+++ unit9/restSpreadExercises/app.js
@@ -15,3 +15,8 @@ const filterOutOdds = (...numbers) => numbers.filter(num => num % 2 === 0);
 // findMin(3,1) // 1
 
 const findMin = (...args) => args.reduce((acc, val) => (acc < val ? acc : val), args[0])
+
+// Write a function called mergeObjects that accepts two objects and returns a new object which contains all the keys and values of the first object and second object.
+// mergeObjects({a:1, b:2}, {c:3, d:4}) // {a:1, b:2, c:3, d:4}
+
+const mergeObjects = (obj1, obj2) => ({...obj1, ...obj2});
