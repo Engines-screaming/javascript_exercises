9eef559763c68ebf6c71abfa7f54bf3e33eae41d
Engines-screaming <karl.ravago@gmail.com>
2021-11-02 17:47:41 -0700

    ccompleted convert.py

--- unit18/convert.py
+++ unit18/convert.py
@@ -1,22 +1,38 @@
 def convert_temp(unit_in, unit_out, temp):
-    """Convert farenheit <-> celsius and return results.
+        """Convert farenheit <-> celsius and return results.
 
-    - unit_in: either "f" or "c" 
-    - unit_out: either "f" or "c"
-    - temp: temperature (in f or c, depending on unit_in)
+        - unit_in: either "f" or "c" 
+        - unit_out: either "f" or "c"
+        - temp: temperature (in f or c, depending on unit_in)
 
-    Return results of conversion, if any.
+        Return results of conversion, if any.
 
-    If unit_in or unit_out are invalid, return "Invalid unit [UNIT_IN]".
+        If unit_in or unit_out are invalid, return "Invalid unit [UNIT_IN]".
 
-    For example:
+        For example:
 
-      convert_temp("c", "f", 0)  =>  32.0
-      convert_temp("f", "c", 212) => 100.0
-    """
+            convert_temp("c", "f", 0)  =>  32.0
+            convert_temp("f", "c", 212) => 100.0
+        """
 
-    # YOUR CODE HERE
+        # YOUR CODE HERE
 
+        # check valid units
+        valid_units = ['c', 'f']
+
+        if unit_in not in valid_units:
+            return f"Invalid unit {unit_in}"
+        
+        if unit_out not in valid_units:
+            return f"Invalid unit {unit_out}"
+
+        # convert
+        if unit_in == unit_out:
+            return temp
+        elif unit_in == 'f':
+            return (temp - 32.0) * (5/9)
+        else:
+            return (temp * 1.8) + 32
 
 print("c", "f", 0, convert_temp("c", "f", 0), "should be 32.0")
 print("f", "c", 212, convert_temp("f", "c", 212), "should be 100.0")
