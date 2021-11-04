be47c77383f4d27a3213255bb866635521d967c7
Engines-screaming <karl.ravago@gmail.com>
2021-11-03 20:47:39 -0700

    completed reverse string

--- unit18/python-ds-practice/05_reverse_string/reverse_string.py
+++ unit18/python-ds-practice/05_reverse_string/reverse_string.py
@@ -7,3 +7,11 @@ def reverse_string(phrase):
         >>> reverse_string('sauce')
         'ecuas'
     """
+
+    # reversed = ''
+    # for x in range(-1, -(len(phrase)) - 1, -1):
+    #     reversed += phrase[x]
+    # return reversed
+
+    # better solution:
+    return phrase[::-1]
