fe3fe06ef3ccbed5aa78373bcb8c1bfe95e3ea4e
Engines-screaming <karl.ravago@gmail.com>
2021-11-03 21:50:15 -0700

    completed letter count

--- unit18/python-ds-practice/07_multiple_letter_count/multiple_letter_count.py
+++ unit18/python-ds-practice/07_multiple_letter_count/multiple_letter_count.py
@@ -7,3 +7,15 @@ def multiple_letter_count(phrase):
         >>> multiple_letter_count('Yay')
         {'Y': 1, 'a': 1, 'y': 1}
     """
+    counts = {}
+
+    for key_letter in phrase:
+
+        if key_letter not in counts.keys():
+            counts[key_letter] = 0  # initialize key letter if it doesnt exist
+
+        for letter in phrase:
+            if key_letter == letter:
+                counts[key_letter] += 1
+
+    return counts
