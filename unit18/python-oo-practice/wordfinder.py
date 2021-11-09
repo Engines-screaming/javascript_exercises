bfb8363aa2f27962bbdab86771cbed005ecf5cb8
Engines-screaming <karl.ravago@gmail.com>
2021-11-09 01:24:57 -0800

    completed wordfinder.py

--- unit18/python-oo-practice/wordfinder.py
+++ unit18/python-oo-practice/wordfinder.py
@@ -1,14 +1,23 @@
 """Word Finder: finds random words from a dictionary."""
-
+import random
 
 class WordFinder:
     """
-    >>> finder = WordFinder('words.txt')
-    235886
+    A class that reads in a list of words and returns a random word
     """
     def __init__(self, words_path):
         self.text = ''
         with open(words_path) as f:
             self.text = f.read()
         self.words = self.text.split("\n")
-        print(f"{len(self.words)}")
+        print(f"{len(self.words)} words read")
+
+    def random(self):
+        rand_index = random.randrange(0, len(self.words))
+        return self.words[rand_index]
+
+
+wf = WordFinder('words.txt')
+print(wf.random())
+print(wf.random())
+print(wf.random())
