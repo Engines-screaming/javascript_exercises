01f32d97ca34f556c3662d43caf31fc4e023454b
Engines-screaming <karl.ravago@gmail.com>
2021-11-09 18:53:39 -0800

    completed wordfinder.py

--- unit18/python-oo-practice/wordfinder.py
+++ unit18/python-oo-practice/wordfinder.py
@@ -10,14 +10,37 @@ class WordFinder:
         with open(words_path) as f:
             self.text = f.read()
         self.words = self.text.split("\n")
-        print(f"{len(self.words)} words read")
 
+    def print_words_len(self):
+        print(f"{len(self.words)} words read")
+        
     def random(self):
         rand_index = random.randrange(0, len(self.words))
         return self.words[rand_index]
 
 
-wf = WordFinder('words.txt')
-print(wf.random())
-print(wf.random())
-print(wf.random())
+class SpecialWordFinder(WordFinder):
+    '''A subclass of wordfinder that can parse txt files with python comments and blank lines'''
+    def __init__(self, words_path):
+        super().__init__(words_path)
+
+        # filter out comments and blank lines
+        def isLineIgnored(value):
+            if value.find('#') >= 0 or len(value) == 0:
+                return False
+            else:
+                return True
+        
+        filtered_words = filter(isLineIgnored, self.words)
+        self.words = [x for x in filtered_words]
+
+# wf = WordFinder('words.txt')
+# print(wf.random())
+# print(wf.random())
+# print(wf.random())
+
+swf = SpecialWordFinder('specialwords.txt')
+print(swf.random())
+print(swf.random())
+print(swf.words)
+
