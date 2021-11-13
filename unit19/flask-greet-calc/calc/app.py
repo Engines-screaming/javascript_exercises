7515141255a5ff3b72d75f600325527854b60219
Engines-screaming <karl.ravago@gmail.com>
2021-11-13 00:13:53 -0800

    calc passes all tests

--- unit19/flask-greet-calc/calc/app.py
+++ unit19/flask-greet-calc/calc/app.py
@@ -22,3 +22,19 @@ def calculate(operation):
         answer = div(a, b)
     
     return str(answer)
+
+@app.route('/math/<operation>')
+def math_route(operation):
+    b = int(request.args['b'])
+    a = int(request.args['a'])
+    
+    if operation == 'add':
+        answer = add(a, b)
+    elif operation == 'sub':
+        answer = sub(a, b)
+    elif operation == 'mult':
+        answer = mult(a, b)
+    elif operation == 'div':
+        answer = div(a, b)
+    
+    return str(answer)
