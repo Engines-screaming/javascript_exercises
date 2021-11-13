dd05e714f1471fd9e5085920e5ee4cece93fb200
Engines-screaming <karl.ravago@gmail.com>
2021-11-13 00:08:09 -0800

    configured pip file and added flask intro demo

--- unit19/flask-intro-demo/app.py
+++ unit19/flask-intro-demo/app.py
@@ -1,3 +1,20 @@
-from flask import Flask
+from flask import Flask, request
 
 app = Flask(__name__)
+
+@app.route('/hello')
+def say_hello():
+    return "HELLO THERE GENERAL KENOBI"
+
+@app.route('/search')
+def return_search():
+    term = request.args['term']
+    sort = request.args['sort']
+
+    return f"""
+    <html>
+    <body>
+      <p>You searched for term={term} and sort={sort}
+    </body>
+    </html>
+    """
