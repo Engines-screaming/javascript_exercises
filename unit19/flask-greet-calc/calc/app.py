183949b38afad4739cbc6ef4bfa5713289c4ca20
Engines-screaming <karl.ravago@gmail.com>
2021-11-13 00:07:19 -0800

    flask calc app is working

--- unit19/flask-greet-calc/calc/app.py
+++ unit19/flask-greet-calc/calc/app.py
@@ -1,1 +1,24 @@
 # Put your app in here.
+from flask import Flask, request
+
+from operations import add, sub, mult, div
+
+
+app = Flask(__name__)
+
+
+@app.route('/<operation>')
+def calculate(operation):
+    b = int(request.args['b'])
+    a = int(request.args['a'])
+    
+    if operation == 'add':
+        answer = add(a, b)
+    elif operation == 'sub':
+        answer = sub(a, b)
+    elif operation == 'mult':
+        answer = mult(a, b)
+    elif operation == 'div':
+        answer = div(a, b)
+    
+    return str(answer)
