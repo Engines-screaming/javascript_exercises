31fcc3451900d9c4a95098e5f0a025d4528d6d8c
Engines-screaming <karl.ravago@gmail.com>
2021-11-16 21:51:33 -0800

    survey front page completed. survey question pages show questions

--- unit19/flask-survey/app.py
+++ unit19/flask-survey/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect
 from surveys import satisfaction_survey
 
 
@@ -12,6 +12,16 @@ responses = []
 def homepage():
     return render_template('survey.html', instructions=satisfaction_survey.instructions)
 
-# @app.route('/question/<int: question_num>')
-# def survey_question(question_num):
-#     pass
+@app.route('/question/<int:question_num>')
+def survey_question(question_num):
+    q = satisfaction_survey.questions[question_num]
+    c = q.choices[question_num]
+    return render_template('question.html', question_num=question_num, question=q.question)
+
+
+
+# @app.route('/submit_question')
+# def submit_question():
+#     '''Stores survey answers and redirects to the next question'''
+    
+    
