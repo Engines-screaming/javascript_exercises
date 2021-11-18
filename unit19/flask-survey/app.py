333d01142c1a166f7057f5c9b6f85d80b6bae27c
Engines-screaming <karl.ravago@gmail.com>
2021-11-17 17:24:17 -0800

    step 3 completed

--- unit19/flask-survey/app.py
+++ unit19/flask-survey/app.py
@@ -1,8 +1,11 @@
 from flask import Flask, render_template, request, redirect
+from flask_debugtoolbar import DebugToolbarExtension
 from surveys import satisfaction_survey
 
-
 app = Flask(__name__)
+app.config['SECRET_KEY'] = "chickenzarecool21837"
+app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
+debug = DebugToolbarExtension(app)
 
 
 # survey variables
@@ -10,22 +13,29 @@ responses = []
 
 @app.route('/')
 def homepage():
-    return render_template('survey.html', 
+    return render_template('survey.html',
+                            survey_title=satisfaction_survey.title,
                             instructions=satisfaction_survey.instructions)
 
 @app.route('/question/<int:question_num>')
 def survey_question(question_num):
+    '''renders the question and choices given an index'''
+
     q = satisfaction_survey.questions[question_num]
     c = q.choices
+    
     return render_template('question.html', 
                             question_num=question_num, 
                             question=q.question,
+                            next_question_num=question_num + 1,
                             choices=c)
 
 
+@app.route('/answer')
+def submit_question():
+    '''Stores survey answers and redirects to the next question'''
+    a = request.args.get('answer')
+    responses.append(a)
+    return redirect(f'/question/{len(responses)}')
 
-# @app.route('/submit_question')
-# def submit_question():
-#     '''Stores survey answers and redirects to the next question'''
-    
     
