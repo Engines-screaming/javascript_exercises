062895235e6f0c859de2d486bcebb7b3f3bca819
Engines-screaming <karl.ravago@gmail.com>
2021-11-23 22:23:43 -0800

    finished survey app

--- unit19/flask-survey/app.py
+++ unit19/flask-survey/app.py
@@ -8,29 +8,28 @@ app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
 debug = DebugToolbarExtension(app)
 
 
-# survey variables
-responses = []
-
 @app.route('/')
 def homepage():
     return render_template('survey.html',
                             survey_title=satisfaction_survey.title,
                             instructions=satisfaction_survey.instructions)
 
-@app.route('/question/<int:question_num>', methods=['POST'])
+@app.route('/question/<int:question_num>')
 def survey_question(question_num):
     '''renders the question and choices given an index'''
-    
-    # start session
-    session['responses'] = []
 
     # if questions are accessed out of order, redirect to the last question that needs an answer
-    if len(responses) != question_num:
+    if len(session['responses']) != question_num:
         flash('You tried to access an invalid question')
-        return redirect(f'/question/{len(responses)}')
+        return redirect(f'/question/{len(session["responses"])}')
     else:
         q = satisfaction_survey.questions[question_num]
         c = q.choices
+
+        print('****************')
+        print(f"{session['responses']}")
+        print(f"{len(session['responses'])}")
+
         return render_template('question.html', 
                                 question_num=question_num, 
                                 question=q.question,
@@ -42,10 +41,19 @@ def survey_question(question_num):
 def submit_question():
     '''Stores survey answers and redirects to the next question'''
     a = request.args.get('answer')
-    responses.append(a)
+
+    r = session['responses']
+    r.append(a)
+    session['responses'] = r
 
     # check if theres more questions, and redirect if there are
-    if len(satisfaction_survey.questions) == len(responses):
+    if len(satisfaction_survey.questions) == len(session['responses']):
         return render_template('finish.html')
     else:
-        return redirect(f'/question/{len(responses)}')
+        return redirect(f'/question/{len(session["responses"])}')
+
+
+@app.route('/start', methods=['POST'])
+def start():
+    session['responses'] = []
+    return redirect('/question/0')
