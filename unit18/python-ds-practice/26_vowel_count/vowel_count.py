7f48da97e5ac5a5812168eb39d9cce7e3f8a4b35
Engines-screaming <karl.ravago@gmail.com>
2021-12-18 20:31:03 -0800

    completed vowel count exercise

--- unit18/python-ds-practice/26_vowel_count/vowel_count.py
+++ unit18/python-ds-practice/26_vowel_count/vowel_count.py
@@ -7,3 +7,18 @@ def vowel_count(phrase):
         >>> vowel_count('HOW ARE YOU? i am great!') 
         {'o': 2, 'a': 3, 'e': 2, 'u': 1, 'i': 1}
     """
+
+    no_spaces = phrase.replace(' ', '')
+    parsed_phrase = no_spaces.lower()
+
+    vowels = ['a', 'e', 'i', 'o', 'u']
+    counts = {}
+    
+    for letter in parsed_phrase:
+        if letter in vowels:
+            if letter in counts:
+                counts[letter] += 1
+            else: 
+                counts[letter] = 1
+    
+    return counts
