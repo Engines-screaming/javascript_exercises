e82b9b3ed90f55a747e2751d0743fe8ba2051dae
Engines-screaming <karl.ravago@gmail.com>
2021-12-19 20:52:03 -0800

    completed join exercises

--- unit22/sql-joins/queries.sql
+++ unit22/sql-joins/queries.sql
@@ -1,1 +1,22 @@
 -- write your queries here
+
+-- Join the two tables so that every column and record appears, regardless of if there is not an owner_id. Your output should look like this:
+select *
+from owners
+    left join vehicles on owners.id = vehicles.owner_id
+
+-- Count the number of cars for each owner. Display the owners first_name, last_name and count of vehicles. The first_name should be ordered in ascending order.
+select first_name, last_name, count(owner_id) as count_of_vehicles
+from owners
+    join vehicles on owners.id = vehicles.owner_id
+group by first_name, last_name
+order by first_name asc
+
+-- Count the number of cars for each owner and display the average price for each of the cars as integers. Display the owners first_name, last_name, average price and count of vehicles. 
+-- The first_name should be ordered in descending order. Only display results with more than one vehicle and an average price greater than 10000.
+select first_name, last_name, avg(price) as avg_car_price, count(owner_id) as count_of_vehicles
+from owners
+    join vehicles on owners.id = vehicles.owner_id
+group by first_name, last_name
+having count(owner_id) > 1 and avg(price) > 10000
+order by first_name desc
