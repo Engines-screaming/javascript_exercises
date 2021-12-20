6fcbe2ae3220cae74770a91cfa61b78b436f3303
Engines-screaming <karl.ravago@gmail.com>
2021-12-19 20:36:57 -0800

    added sql join practice

--- unit22/sql-querying/queries_products.sql
+++ unit22/sql-querying/queries_products.sql
@@ -19,24 +19,30 @@ SELECT * FROM products
 SELECT name FROM products
 
 -- Display all of the names and prices of the products.
-SELECT name, price FROM products
+SELECT name, price FROM products;
 
 -- Add a new product - make up whatever you would like!
 INSERT INTO products (id, name, price, can_be_returned)
-    VALUES (3, 'tv', 300.00, true)
+    VALUES (3, 'tv', 300.00, true);
 
 -- Display only the products that can_be_returned.
 SELECT name FROM products
-WHERE can_be_returned = true
+WHERE can_be_returned = true;
 
 -- Display only the products that have a price less than 44.00.
-SELECT name FROM products WHERE price < 44.00
+SELECT name FROM products WHERE price < 44.00;
 
 -- Display only the products that have a price in between 22.50 and 99.99.
-SELECT name FROM products WHERE price between 22.50 and 99.99
+SELECT name FROM products WHERE price between 22.50 and 99.99;
 
 -- There’s a sale going on: Everything is $20 off! Update the database accordingly.
+UPDATE products SET price = price - 20;
 
 -- Because of the sale, everything that costs less than $25 has sold out. Remove all products whose price meets this criteria.
+DELETE FROM products WHERE price < 25;
+
 -- And now the sale is over. For the remaining products, increase their price by $20.
+UPDATE products SET price = price + 20;
+
 -- There is a new company policy: everything is returnable. Update the database accordingly.
+UPDATE products SET can_be_returned = true
