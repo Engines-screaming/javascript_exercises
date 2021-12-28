06081e6908c9a7ababa695ea6ead76e97407a5a1
Engines-screaming <karl.ravago@gmail.com>
2021-12-27 17:50:27 -0800

    completed outer space db revisions

--- unit22/sql-ddl-design/outer_space.sql
+++ unit22/sql-ddl-design/outer_space.sql
@@ -13,16 +13,42 @@ CREATE TABLE planets
   name TEXT NOT NULL,
   orbital_period_in_years FLOAT NOT NULL,
   orbits_around TEXT NOT NULL,
-  galaxy TEXT NOT NULL,
-  moons TEXT[]
+  galaxy TEXT NOT NULL
 );
 
+-- ****** ORIGINAL CODE ********
+-- INSERT INTO planets
+--   (name, orbital_period_in_years, orbits_around, galaxy, moons)
+-- VALUES
+--   ('Earth', 1.00, 'The Sun', 'Milky Way', '{"The Moon"}'),
+--   ('Mars', 1.88, 'The Sun', 'Milky Way', '{"Phobos", "Deimos"}'),
+--   ('Venus', 0.62, 'The Sun', 'Milky Way', '{}'),
+--   ('Neptune', 164.8, 'The Sun', 'Milky Way', '{"Naiad", "Thalassa", "Despina", "Galatea", "Larissa", "S/2004 N 1", "Proteus", "Triton", "Nereid", "Halimede", "Sao", "Laomedeia", "Psamathe", "Neso"}'),
+--   ('Proxima Centauri b', 0.03, 'Proxima Centauri', 'Milky Way', '{}'),
+--   ('Gliese 876 b', 0.23, 'Gliese 876', 'Milky Way', '{}');
+
+-- I think we can improve the database by making a separate table of moons and a separate table for mapping moons to planets
 INSERT INTO planets
-  (name, orbital_period_in_years, orbits_around, galaxy, moons)
+  (name, orbital_period_in_years, orbits_around, galaxy)
 VALUES
-  ('Earth', 1.00, 'The Sun', 'Milky Way', '{"The Moon"}'),
-  ('Mars', 1.88, 'The Sun', 'Milky Way', '{"Phobos", "Deimos"}'),
-  ('Venus', 0.62, 'The Sun', 'Milky Way', '{}'),
-  ('Neptune', 164.8, 'The Sun', 'Milky Way', '{"Naiad", "Thalassa", "Despina", "Galatea", "Larissa", "S/2004 N 1", "Proteus", "Triton", "Nereid", "Halimede", "Sao", "Laomedeia", "Psamathe", "Neso"}'),
-  ('Proxima Centauri b', 0.03, 'Proxima Centauri', 'Milky Way', '{}'),
-  ('Gliese 876 b', 0.23, 'Gliese 876', 'Milky Way', '{}');
+  ('Earth', 1.00, 'The Sun', 'Milky Way'),
+  ('Mars', 1.88, 'The Sun', 'Milky Way'),
+  ('Venus', 0.62, 'The Sun', 'Milky Way'),
+  ('Neptune', 164.8, 'The Sun', 'Milky Way'),
+  ('Proxima Centauri b', 0.03, 'Proxima Centauri', 'Milky Way'),
+  ('Gliese 876 b', 0.23, 'Gliese 876', 'Milky Way');
+
+-- *** improved tables
+CREATE TABLE moons (
+  id SERIAL PRIMARY KEY NOT NULL,
+  moon_name TEXT NOT NULL,
+  planet_id REFERENCES planets(id)
+)
+
+INSERT INTO moons
+  (moon_name, planet_id)
+VALUES
+  ('The Moon', 1),
+  ('Phobos', 2),
+  ('Deimos', 2),
+  ('Naiad', 4)
