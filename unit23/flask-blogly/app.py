d8b7cbc36a9e917de6e2883623cfa0ff0e892550
Engines-screaming <karl.ravago@gmail.com>
2022-01-08 23:52:32 -0800

    created route to list all users

--- unit23/flask-blogly/app.py
+++ unit23/flask-blogly/app.py
@@ -1,16 +1,23 @@
 """Blogly application."""
 
 from flask import Flask, render_template
-from models import db, connect_db
+from models import db, connect_db, User
+from flask_debugtoolbar import DebugToolbarExtension
+
 
 app = Flask(__name__)
+
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql:///blogly'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SECRET_KEY'] = "VERYSECRETKEY!"
 app.config['SQLALCHEMY_ECHO'] = True
 
+debug = DebugToolbarExtension(app)
+
 connect_db(app)
 
 @app.route('/')
 def home():
-    render_template('base.html')
+    users = User.query.all()
+
+    return render_template('list.html', users=users)
