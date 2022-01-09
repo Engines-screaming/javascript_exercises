61d6332fe970951e812e0ac1d0bd0ec432cb4a96
Engines-screaming <karl.ravago@gmail.com>
2022-01-08 23:50:44 -0800

    working seed file

--- unit23/flask-blogly/seed.py
+++ unit23/flask-blogly/seed.py
@@ -3,24 +3,20 @@
 from models import User, db
 from app import app
 
-# Create all tables
+# if exists drop and create
 db.drop_all()
 db.create_all()
 
-# If table isn't empty, empty it
 User.query.delete()
 
-# # Add users
+# create users
 zucc = User(first_name='Mark', last_name='Zuckerberg', image_url='https://en.wikipedia.org/wiki/File:Mark_Zuckerberg_F8_2019_Keynote_(32830578717)_(cropped).jpg')
+lebron = User(first_name='LeBron', last_name='James', image_url='https://upload.wikimedia.org/wikipedia/commons/thumb/c/cf/LeBron_James_crop.jpg/220px-LeBron_James_crop.jpg')
+jack = User(first_name='Jack', last_name='Black', image_url='https://upload.wikimedia.org/wikipedia/commons/thumb/3/3c/Jack_Black_2011_2.jpg/340px-Jack_Black_2011_2.jpg')
 
-# whiskey = Pet(name='Whiskey', species="dog")
-# bowser = Pet(name='Bowser', species="dog", hunger=10)
-# spike = Pet(name='Spike', species="porcupine")
-
-# # Add new objects to session, so they'll persist
+# Add new objects to session
 db.session.add(zucc)
-# db.session.add(bowser)
-# db.session.add(spike)
+db.session.add(lebron)
+db.session.add(jack)
 
-# Commit--otherwise, this never gets saved!
 db.session.commit()
