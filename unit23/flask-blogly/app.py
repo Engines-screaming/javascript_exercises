eb6db0027a2acfe5282df9d7df319df108ec568e
Engines-screaming <karl.ravago@gmail.com>
2022-01-09 20:33:51 -0800

    implemented edit page but does not update database

--- unit23/flask-blogly/app.py
+++ unit23/flask-blogly/app.py
@@ -17,12 +17,25 @@ debug = DebugToolbarExtension(app)
 connect_db(app)
 
 @app.route('/')
+@app.route('/users')
 def home():
     users = User.query.all()
     return render_template('list.html', users=users)
 
 
-@app.route('/user/<int:user_id>')
+@app.route('/users/<int:user_id>')
 def show_details(user_id):
     user = User.query.get_or_404(user_id)
     return render_template('details.html', user=user)
+
+
+@app.route('/users/<int:user_id>/edit', methods=['GET'])
+def edit(user_id):
+    user = User.query.get_or_404(user_id)
+    return render_template('edit.html', user=user)
+
+
+# @app.route('/users/<int:user_id>/edit', methods=['POST'])
+# def edit(user_id):
+#     # TODO: process the edit form returning the user to the /users page
+#     pass
