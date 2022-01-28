a6b89c927764792016289e5d8b98a250e42ad0ce
Engines-screaming <karl.ravago@gmail.com>
2022-01-28 02:47:13 -0800

    init commit

---
+++ unit24/flask-feedback/app.py
@@ -0,0 +1,15 @@
+from flask import Flask, render_template, flash, redirect, render_template
+from flask_debugtoolbar import DebugToolbarExtension
+from models import db, connect_db,
+
+from forms import AddSnackForm
+from forms import UserForm
+
+app = Flask(__name__)
+app.config["SECRET_KEY"] = "oh-so-secret"
+app.config["SQLALCHEMY_DATABASE_URI"] = "postgresql:///feedback"
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+
+debug = DebugToolbarExtension(app)
+
+connect_db(app)
