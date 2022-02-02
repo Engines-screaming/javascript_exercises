995e1ecd0911cb2827968d1b543f7aef8a85a453
Engines-screaming <karl.ravago@gmail.com>
2022-02-01 23:52:35 -0800

    working models and seed file

--- unit24/flask-cupcakes/app.py
+++ unit24/flask-cupcakes/app.py
@@ -1,1 +1,15 @@
 """Flask app for Cupcakes"""
+
+from flask import Flask, render_template, redirect
+from flask_debugtoolbar import DebugToolbarExtension
+from models import db, connect_db, Cupcake
+
+
+app = Flask(__name__)
+app.config["SECRET_KEY"] = "oh-so-secret"
+app.config["SQLALCHEMY_DATABASE_URI"] = "postgresql:///cupcakes"
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+
+debug = DebugToolbarExtension(app)
+
+connect_db(app)
