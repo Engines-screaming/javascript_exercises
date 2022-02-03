bfb56835f5a39f5d71a0120b35edd705d32de77f
Engines-screaming <karl.ravago@gmail.com>
2022-02-02 23:39:01 -0800

    working get /api/cupcakes route

--- unit24/flask-cupcakes/app.py
+++ unit24/flask-cupcakes/app.py
@@ -1,6 +1,6 @@
 """Flask app for Cupcakes"""
 
-from flask import Flask, render_template, redirect
+from flask import Flask, render_template, redirect, jsonify, request
 from flask_debugtoolbar import DebugToolbarExtension
 from models import db, connect_db, Cupcake
 
@@ -13,3 +13,35 @@ app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 debug = DebugToolbarExtension(app)
 
 connect_db(app)
+
+
+@app.route('/api/cupcakes')
+def list_cupcakes():
+    cupcakes = [cupcake.serialize() for cupcake in Cupcake.query.all()]
+
+    # import pdb
+    # pdb.set_trace()
+
+    return jsonify({'cupcakes': cupcakes})
+
+@app.route('/api/cupcakes', methods=['POST'])
+def create_cupcake():
+    cupcake_dict = {
+        'id': request.data['id'],
+        'flavor': request.data['flavor'],
+        'size': request.data['size'],
+        'rating': request.data['rating'],
+        'image': request.data['image']
+    }
+
+    cupcake = Cupcake(**cupcake_dict)
+    db.session.add(cupcake)
+    db.session.commit()
+    return jsonify(cupcake.serialize())
+
+@app.route('/api/cupcakes/<int:cupcake_id>')
+def cupcake_details(cupcake_id):
+    cupcake = Cupcake.query.get_or_404(cupcake_id)
+    return jsonify(cupcake.serialize())
+
+
