e3d19f407782c1055a47e1853859add96db0b89b
Engines-screaming <karl.ravago@gmail.com>
2022-05-03 22:55:41 -0700

    make step2.js take values from cmd line

--- unit31/node-files/step2.js
+++ unit31/node-files/step2.js
@@ -1,15 +1,25 @@
 
-const fs = require("fs")
-
+const fs = require("fs");
+const axios = require("axios");
 
 function cat(path) {
     fs.readFile(path, "utf-8", (err, data) => {
         if (err) {
-            console.log(`Error reading ${path}: ${err}`)
+            console.log(`Error reading ${path}: ${err}`);
         }
-        console.log(data)
+        console.log(data);
     })
 }
 
-cat("one.txt")
-cat("onesdfasdf.txt")
+function webCat(url) {
+    axios.get(url)
+        .then((resp) => {
+            console.log(resp.data);
+        })
+        .catch((err) => {
+            console.log(`error fetching ${url}: \n${err}`);
+        })
+}
+
+const argv = process.argv
+webCat(argv[2]);
