bb2e1b6824c27952c5581fc114934b0feb91c758
Engines-screaming <karl.ravago@gmail.com>
2022-05-03 22:55:18 -0700

    make step1.js take values from cmd line

--- unit31/node-files/step1.js
+++ unit31/node-files/step1.js
@@ -17,15 +17,15 @@
 
 const fs = require("fs");
 
-
 function cat(path) {
     fs.readFile(path, "utf-8", (err, data) => {
         if (err) {
-            console.log(`Error reading ${path}: ${err}`);
+            console.log(`Error reading ${path}: \n${err}`);
         }
         console.log(data);
     })
 }
 
-cat("one.txt");
-cat("onesdfasdf.txt");
+
+const argv = process.argv
+cat(process.argv[2]);
