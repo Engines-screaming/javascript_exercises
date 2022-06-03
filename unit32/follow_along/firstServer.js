ea6df91137f138bfd0c091a2981fd5bfee6360ca
Engines-screaming <karl.ravago@gmail.com>
2022-06-03 00:07:14 -0700

    reorganized

--- unit32/follow_along/firstServer.js
+++ unit32/follow_along/firstServer.js
@@ -2,6 +2,12 @@ const express = require('express');
 
 app = express();
 
+app.get('/', (req, res) => {
+    return res.send('welcome home');
+});
+
+
+
 app.listen(3000, function() {
     console.log("listening on port 3000");
-}) 
+});
