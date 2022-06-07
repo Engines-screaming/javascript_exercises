4014556dee108ae907f10e494e7ed44603078e3e
Engines-screaming <karl.ravago@gmail.com>
2022-06-07 00:07:44 -0700

    removed eslint and added routing and middleware follow along

--- unit32/follow_along/firstServer.js
+++ unit32/follow_along/firstServer.js
@@ -2,11 +2,18 @@ const express = require('express');
 
 app = express();
 
+
 app.get('/', (req, res) => {
-    return res.send('welcome home');
+    return res.send('welcome trainer');
 });
 
+app.get('/bulbasaur', (req, res) => {
+    return res.send('<h1>BULBASAUR</h1>');
+});
 
+app.post('/bulbasaur', (req, res) => {
+    return res.send('<h1>YOU CAUGHT A BULBASAUR!!!</h1>');
+});
 
 app.listen(3000, function() {
     console.log("listening on port 3000");
