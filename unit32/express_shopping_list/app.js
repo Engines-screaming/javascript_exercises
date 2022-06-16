907e96e7dd86bb3db7bce7d8b1ae4645e2c304bd
Engines-screaming <karl.ravago@gmail.com>
2022-06-16 00:30:41 -0700

    implemented items get route

--- unit32/express_shopping_list/app.js
+++ unit32/express_shopping_list/app.js
@@ -3,3 +3,10 @@ const express = require('express');
 const app = express();
 app.use(express.json());
 
+// import routes
+const routes = require('./routes');
+app.use('/', routes);
+
+app.listen('3000', function(){
+    console.log('server starting on port 3000')
+})
--- unit32/express_shopping_list/routes.js
+++ unit32/express_shopping_list/routes.js
@@ -1,5 +1,28 @@
 const express = require('express');
 const router = new express.Router();
 
+// fake db
+const items = require('./fakeDB');
+
+router.get('/items', (req, res) => {
+    // TODO
+    return res.json(items);
+});
+
+// router.post('/items', (req, res) => {
+//     // TODO
+// });
+
+// router.get('/items/:name', (req, res) => {
+//     // TODO
+// });
+
+// router.patch('/items/:name', (req, res) => {
+//     // TODO
+// });
+
+// router.delete('/items/:name', (req, res) => {
+//     // TODO
+// });
 
 module.exports = router;
