6fa91cfbba249ad4871c8521a28aefc33159c63c
Engines-screaming <karl.ravago@gmail.com>
2022-06-30 01:18:14 -0700

    add lunchly files

--- unit35/express-biztime/routes/invoices.js
+++ unit35/express-biztime/routes/invoices.js
@@ -52,31 +52,37 @@ router.post('/', async function(req, res, next) {
 
 // PUT /invoices/[id]
 // Updates an invoice.
-
 // If invoice cannot be found, returns a 404.
-
 // Needs to be passed in a JSON body of {amt}
-
 // Returns: {invoice: {id, comp_code, amt, paid, add_date, paid_date}}
+router.put('/:id', async function(req, res, next) {
+  try {
+    const id = req.params.id;
+    const result = await db.query(`UPDATE invoices SET amt=$1 WHERE id = $2
+                                   RETURNING id, comp_code, amt, paid, add_date, paid_date`, 
+                                   [amt, id]
+    );
+    return res.json({invoice: result.rows[0]});
+  } catch(e) {
+    return next(e);
+  }
+});
 
-
-
-
-// DELETE /invoices/[id]
-// Deletes an invoice.
-
-// If invoice cannot be found, returns a 404.
-
-// Returns: {status: "deleted"}
-
-// Also, one route from the previous part should be updated:
-
-
+router.delete('/:id', async function(req, res, next) {
+  try {
+    const id = req.params.id;
+    const results = await db.query(`DELETE FROM invoices WHERE id = $1`, [id]);
+    return res.json({status: "deleted"});
+  } catch(e) {
+    next(e);
+  }
+});
 
 
 // GET /companies/[code]
 // Return obj of company: {company: {code, name, description, invoices: [id, ...]}}
-
 // If the company given cannot be found, this should return a 404 status response.
+
+
 
 module.exports = router;
