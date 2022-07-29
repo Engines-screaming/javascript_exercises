e53c36f8f10f04c709315a5f527ab22c240f583e
Engines-screaming <karl.ravago@gmail.com>
2022-07-29 01:09:07 -0700

    completed helpers

--- unit39/es2015exercise/myapp/src/helpers.js
+++ unit39/es2015exercise/myapp/src/helpers.js
@@ -7,7 +7,7 @@ function choice(items) {
 
 // remove(items, item): removes the first matching item from items, if item exists, and returns it. Otherwise returns undefined.
 function remove(items, item) {
-
+    return items.filter((val) => { val !== item });
 }
 
 export { choice, remove }
