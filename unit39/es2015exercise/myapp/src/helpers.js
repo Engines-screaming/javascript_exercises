45b2eef1419efcaba6bcd91b0a21edc8ac5f1659
Engines-screaming <karl.ravago@gmail.com>
2022-07-29 00:47:59 -0700

    initial exercise

---
+++ unit39/es2015exercise/myapp/src/helpers.js
@@ -0,0 +1,13 @@
+
+// choice(items): returns a randomly selected item from array of items
+function choice(items) {
+    let randIdx = Math.floor(Math.random() * items.length);
+    return items[randIdx];
+}
+
+// remove(items, item): removes the first matching item from items, if item exists, and returns it. Otherwise returns undefined.
+function remove(items, item) {
+
+}
+
+export { choice, remove }
