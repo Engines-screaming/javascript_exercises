3c8e65a7c012e97ffedce34c9255865fd122e94a
Engines-screaming <karl.ravago@gmail.com>
2022-09-10 12:58:32 -0700

    tests pass for constructNote

--- unit48/problemSolvingExercises/myapp/src/constructNote.test.js
+++ unit48/problemSolvingExercises/myapp/src/constructNote.test.js
@@ -1,15 +1,16 @@
-import constructNote from './constructNote.js';
+import constructNote from './constructNote';
 
-describe('Testing constructNote', () => {
+describe('testing constructNote function', () => {
     test('test #1', () => {
         expect(constructNote('aa', 'abc')).toBe(false);
-    });
-
+    })
+    
     test('test #2', () => {
         expect(constructNote('abc', 'dcba')).toBe(true);
-    });
+    })
 
     test('test #3', () => {
         expect(constructNote('aabbcc', 'bcabcaddff')).toBe(true);
-    });
+    })
+    
 })
