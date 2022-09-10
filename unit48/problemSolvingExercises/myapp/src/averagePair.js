d653eb2601d054a9fa287c7086fd1a72ed819276
Engines-screaming <karl.ravago@gmail.com>
2022-09-10 13:28:47 -0700

    optimized solution for average pair

--- unit48/problemSolvingExercises/myapp/src/averagePair.js
+++ unit48/problemSolvingExercises/myapp/src/averagePair.js
@@ -1,9 +1,28 @@
+// function averagePair(inputArr, target) {
+//     for (let i = 0; i < inputArr.length; i++) {
+//         for (let j = i + 1; j < inputArr.length; j++) {
+//             if ((inputArr[i] + inputArr[j])/2 === target) {
+//                 return true;
+//             }
+//         }
+//     }
+//     return false;
+// }
+
+
 function averagePair(inputArr, target) {
-    for (let i = 0; i < inputArr.length; i++) {
-        for (let j = i + 1; j < inputArr.length; j++) {
-            if ((inputArr[i] + inputArr[j])/2 === target) {
-                return true;
-            }
+    // optimized solution with pointers method
+    let lptr = 0;
+    let rptr = inputArr.length - 1;
+
+    while (lptr < rptr) {
+        let currentAvg = (inputArr[lptr] + inputArr[rptr]) / 2;
+        if (currentAvg === target) {
+            return true;
+        } else if (currentAvg < target) {
+            lptr++;
+        } else if (currentAvg > target) {
+            rptr--;
         }
     }
     return false;
