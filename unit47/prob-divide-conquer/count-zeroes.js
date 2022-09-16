63bfcc1bc5301994f28ebf368c74b8e242f86904
Engines-screaming <karl.ravago@gmail.com>
2022-09-15 23:22:21 -0700

    count-zeros pass tests

--- unit47/prob-divide-conquer/count-zeroes.js
+++ unit47/prob-divide-conquer/count-zeroes.js
@@ -1,5 +1,23 @@
-function countZeroes() {
-  
+function countZeroes(inputArr) {
+    // define zeros_couter = 0
+    let zerosCounter = 0;
+    let middleIdx;
+    if (inputArr.length % 2 === 0) {
+        middleIdx = Math.floor(inputArr.length / 2);
+        for (let i = 0; i < middleIdx; i++) {
+            if (inputArr[i] === 0) zerosCounter++;
+            if (inputArr[middleIdx + i] === 0) zerosCounter++;
+        }
+    } else {
+        const iterations = Math.floor((inputArr.length - 1) / 2);
+        middleIdx = Math.ceil((inputArr.length - 1) / 2);
+        if (inputArr[middleIdx] === 0) zerosCounter++;
+        for (let i = 0; i < iterations; i++) {
+            if (inputArr[middleIdx + i + 1] === 0) zerosCounter++;
+            if (inputArr[middleIdx - (i + 1)] === 0) zerosCounter++;
+        }
+    }
+    return zerosCounter;
 }
 
 module.exports = countZeroes
