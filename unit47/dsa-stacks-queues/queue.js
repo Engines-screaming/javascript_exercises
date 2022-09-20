6fc6be462788fd737d5ff04fec0d8d7da2524671
Engines-screaming <karl.ravago@gmail.com>
2022-09-19 20:24:23 -0700

    stacks and queues pass tests

--- unit47/dsa-stacks-queues/queue.js
+++ unit47/dsa-stacks-queues/queue.js
@@ -20,26 +20,41 @@ class Queue {
   /** enqueue(val): add new value to end of the queue. Returns undefined. */
 
   enqueue(val) {
-
+    const newNode = new Node(val);
+    if (this.first === null) {
+      this.first = newNode;
+      this.last = newNode;
+      this.size += 1;
+    } else {
+      this.last.next = newNode;
+      this.last = newNode;
+      this.size += 1;
+    }
   }
 
   /** dequeue(): remove the node from the start of the queue
    * and return its value. Should throw an error if the queue is empty. */
 
   dequeue() {
+    if (this.first === null) throw new Error('queue is empty!');
 
+    const oldFirst = this.first;
+    const newFirst = this.first.next;
+    this.first = newFirst;
+    this.size -= 1;
+    return oldFirst.val;
   }
 
   /** peek(): return the value of the first node in the queue. */
 
   peek() {
-
+    return this.first.val;
   }
 
   /** isEmpty(): return true if the queue is empty, otherwise false */
 
   isEmpty() {
-
+    return this.first === null;
   }
 }
 
